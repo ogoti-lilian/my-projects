3119d5f0d823dff945982654fbf2db8e2fab48ae
ogoti-lilian <ogotililian20@gmail.com>
2021-09-28 18:07:19 +0300

    commit

---
+++ aspnetcorewebapinuxtapp/nuxtApp/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <td>
+    <h2>Index Page</h2>
+    {{ res_display }}
+  </td>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        res_display: []
+      }
+    },
+    async mounted() {
+      const res_display = await this.$axios.$get('weatherforecast')
+      this.res_display = res_display
+    }
+  }
+</script>
+
